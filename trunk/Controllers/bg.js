440bad341a98d5330ea4adb7fa899575644c1d5d
mohamedaliazouz <mohamedaliazouz@7c063873-07dd-0e2c-0e23-6587fe7ed649>
2011-02-27 14:45:31 +0000

    add childeren for share context menus 1 copy shortener url second to send url page to bakground page

--- trunk/Controllers/bg.js
+++ trunk/Controllers/bg.js
@@ -2,9 +2,10 @@ ShareLinksBG={
     selectedText:"",
     pageurl:"",
     init:function(){
-      ShareLinksBG.createContextMenu();
+        ShareLinksBG.createMainContextMenu();
+        ShareLinksBG.createContextMenu("share");
     },
-    createContextMenu:function(){
+    createMainContextMenu:function(){
         createProperties={
             "title":"text",
             "contexts":["selection"],
@@ -16,13 +17,70 @@ ShareLinksBG={
         chrome.contextMenus.create(createProperties);
         createProperties={
             "title":"share",
+            "contexts":["page"]
+        };
+        SharingStaticData.sharingParentID=chrome.contextMenus.create(createProperties);
+        ShareLinksBG.setData(SharingStaticData)
+        createProperties={
+            "title":"Copy Shortener url",
             "contexts":["page"],
+            "onclick":function(OnClickData,tab){
+                ShareLinksBG.getShortenerUrl(tab.url)
+            },
+            "parentId":SharingStaticData.sharingParentID
+        };
+        chrome.contextMenus.create(createProperties);
+    },
+    setData:function(Data){
+        localStorage.sharingStaticData=JSON.stringify(Data);
+    },
+    getdata:function(){
+        return localStorage.sharingStaticData;
+    },
+    createContextMenu:function(name){
+        createProperties={
+            "title":name,
+            "contexts":["all"],
             "onclick":function(OnClickData,tab){
                 ShareLinksBG.pageurl=tab.url
                 console.log("URL:"+ShareLinksBG.pageurl)
-            }
+                alert(tab.url)
+            },
+            "parentId":SharingStaticData.sharingParentID
         };
         chrome.contextMenus.create(createProperties);
+    },
+    copyurl:function(text){
+        var input = document.getElementById('shorturl');
+        input.value =text;
+        input.select();
+        document.execCommand('Copy');
+    },
+    getShortenerUrl:function(url){
+        var response;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open("POST", "http://goo.gl/api/shorten?url=" +encodeURIComponent(url), false);
+
+        xmlhttp.onload = function()
+        {
+            var object = JSON.parse(xmlhttp.responseText);
+
+            if(object["short_url"] == undefined)
+                response = {
+                    status: "error",
+                    message: object["error_message"]
+                };
+            else
+                response = {
+                    status: "success",
+                    message: object["short_url"]
+                };
+        }
+        xmlhttp.send(null);
+        ShareLinksBG.copyurl(response.message);
+    },
+    share:function(){
+        alert("share......................")
     }
 }
 $(function(){

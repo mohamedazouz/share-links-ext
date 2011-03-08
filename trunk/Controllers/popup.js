881f63ae64615bd18ad7cc04bc190c63f92bfcae
mohamedaliazouz <mohamedaliazouz@7c063873-07dd-0e2c-0e23-6587fe7ed649>
2011-03-08 14:14:52 +0000

    share to facebook function

--- trunk/Controllers/popup.js
+++ trunk/Controllers/popup.js
@@ -10,6 +10,11 @@ SharingPopup={
 
         url="http://41.178.64.38:80/sharing_proxy/get_fb_token.php"
 
+    },
+    share:function(){
+        chrome.tabs.getSelected(null,function(tab){
+            chrome.extension.getBackgroundPage().ShareLinksBG.sharefacebook($("#msg").val(), tab.url);
+        })
     }
 }
 $(function(){

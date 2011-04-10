fd907eaebe720f508859232b4189e2f2a24659ef
mohamedaliazouz <mohamedaliazouz@7c063873-07dd-0e2c-0e23-6587fe7ed649>
2011-04-10 10:38:15 +0000

    Final extension with all CSS

--- trunk/views/content_script/contentscript.js
+++ trunk/views/content_script/contentscript.js
@@ -5,76 +5,163 @@ var container;
 script={
     show:function(onclickedcontext){
         if(!container){
+            site=JSON.parse(onclickedcontext);
+            //extension container ui
             container  = document.createElement('div');
-            container.setAttribute("class", "container");
+            container.setAttribute("class", "share-container f");
             container.setAttribute("id", "container");
-        
-            var logo  = document.createElement('div');
-            logo.setAttribute("class", "logo f");
+            container.setAttribute("style", "background:url("+chrome.extension.getURL("views/images/link_popup1.png")+") no-repeat;top: 57px;right: 10px;position: absolute;z-index: 200;");
 
+            //exte logo div
+            var logo  = document.createElement('div');
+            logo.setAttribute("class", "share-logo f");
+
+            //ext logo image
             var img  = document.createElement('img');
             img.setAttribute("src", chrome.extension.getURL("views/images/logo.png"));
             img.setAttribute("width",76);
             img.setAttribute("height",37);
+            logo.appendChild(img);
 
+            //shared item
             var link  = document.createElement('div');
             link.setAttribute("class", "link-cont f-r");
             link.setAttribute("id", "shareitem");
 
-            var msg  = document.createElement('textarea');
-            msg.setAttribute("class", "txt-area");
-            msg.setAttribute("id", "msg");
-
+            //close container
+            var close  = document.createElement('a');
+            close.setAttribute("class", "share-close");
+            close.innerHTML="X";
+            close.onclick=function(){
+                script.fade(-1)
+            }
+            //share button
             var button  = document.createElement('input');
             button.setAttribute("type", "submit");
             button.setAttribute("class", "send-btn f-r");
             button.setAttribute("value", "share");
 
-            var desc  = document.createElement('div');
-            desc.setAttribute("id", "desc");
 
-            site=JSON.parse(onclickedcontext);
+            container.appendChild(close);
+            container.appendChild(logo);
+            container.appendChild(link);
+            
+
+            
+
+
             out="</br><a href='' class='f-r'><img src='"+ chrome.extension.getURL("views/images/"+site.value+".png")+"' width='31' height='32' alt='"+site.value+"' /></a>";
             out+="<a href='' class='"+site.value+" f'>"+site.name+"</a>";
             if(site.text){
                 msg.value=site.text;
             }
             link.innerHTML=out;
+            var JsonData;
+            if(site.value=="gmail"){
+                // message to
+                var messageFieldTO  = document.createElement('input');
+                messageFieldTO.setAttribute("class", "messageField");
+                messageFieldTO.setAttribute("type", "text");
 
-            out="<table>"
-            out="<tr><td><b>Image : </b></td><td><img src='"+site.favIconUrl+"' /></td></tr>"
-            out+="<tr><td><b>Title :</b></td><td>"+site.pagetitle+"</td></tr>";
-            out+="<tr><td><b>Url  : </b></td><td>"+site.url+"</td></tr>";
+
+                //message subject
+                var messageFieldSubject  = document.createElement('input');
+                messageFieldSubject.setAttribute("class", "messageField");
+                messageFieldSubject.setAttribute("type", "text");
+
+                //commment msg
+                var msg  = document.createElement('textarea');
+                msg.setAttribute("class", "message-area");
+                
+                
+                container.appendChild(messageFieldTO)
+                container.appendChild(messageFieldSubject);
+                container.appendChild(msg);
+
+                button.onclick=function(){
+                    type=site.value;
+                    json={
+                       'share': 'done',
+                        type:type,
+                        msg:msg.value,
+                        url:site.url,
+                        to:messageFieldTO.value,
+                        sub:messageFieldSubject.value
+                    }
+                    chrome.extension.sendRequest(json, sucess);
+                    function sucess(back){
+                        alert(back)
+                        script.fade(-1)
+                    }
+                }
+                
+
+            }else{
+                //commment msg
+                var msg  = document.createElement('textarea');
+                msg.setAttribute("class", "txt-area");
+                msg.setAttribute("id", "msg");
+
+              
+                var shareTitle=document.createElement('p');
+                shareTitle.setAttribute("class", "share-title");
+                shareTitle.innerHTML=site.pagetitle;
+
+
+                var shareTxt=document.createElement('div');
+                shareTxt.setAttribute("class", "share-txt f-r");
+                shareTxt.innerHTML=site.url;
+                var close_shareTxt  = document.createElement('a');
+                close_shareTxt.setAttribute("class", "share-close");
+                close_shareTxt.innerHTML="X";
+                shareTxt.appendChild(close_shareTxt);
+                close_shareTxt.onclick=function(){
+                    container.removeChild(shareTxt);
+                }
+
             
-            desc.innerHTML=out;
 
-            button.onclick=function(){
-                type=site.value;
-                json={
-                    'share': 'done',
-                    type:type,
-                    msg:msg.value,
-                    url:site.url
+            
+                var shareImage=document.createElement('div');
+                shareImage.setAttribute("class", "share-image");
+                var shareImage_img  = document.createElement('img');
+                shareImage_img.setAttribute("src", site.favIconUrl);
+                shareImage.appendChild(shareImage_img);
+                var shareImage_close  = document.createElement('a');
+                shareImage_close.setAttribute("class", "share-close");
+                shareImage_close.innerHTML="X";
+                shareImage.appendChild(shareImage_close);
+                shareImage_close.onclick=function(){
+                    container.removeChild(shareImage);
                 }
-                chrome.extension.sendRequest(json, sucess);
-                function sucess(back){
-                    alert(back)
-                    script.fade(-1)
+
+
+                container.appendChild(msg);
+                container.appendChild(shareTitle);
+                container.appendChild(shareTxt);
+                container.appendChild(shareImage);
+
+                
+                button.onclick=function(){
+                    type=site.value;
+                    json={
+                        'share': 'done',
+                        type:type,
+                        msg:msg.value,
+                        url:site.url,
+                        img:site.favIconUrl,
+                        des:site.url
+
+                    }
+                    chrome.extension.sendRequest(json, sucess);
+                    function sucess(back){
+                        alert(back)
+                        script.fade(-1)
+                    }
                 }
             }
-            var close  = document.createElement('a');
-            close.innerHTML="X";
-            close.onclick=function(){
-                script.fade(-1)
-            }
-
-            logo.appendChild(img);
-            container.appendChild(close);
-            container.appendChild(logo);
-            container.appendChild(link);
-            container.appendChild(msg);
             container.appendChild(button);
-            container.appendChild(desc);
+            
             document.body.appendChild(container);
         }
     },
@@ -101,3 +188,38 @@ script={
     }
 
 }
+
+
+
+            /*var desc  = document.createElement('div');
+            desc.setAttribute("id", "desc");
+
+            site=JSON.parse(onclickedcontext);
+            out="</br><a href='' class='f-r'><img src='"+ chrome.extension.getURL("views/images/"+site.value+".png")+"' width='31' height='32' alt='"+site.value+"' /></a>";
+            out+="<a href='' class='"+site.value+" f'>"+site.name+"</a>";
+            if(site.text){
+                msg.value=site.text;
+            }
+            link.innerHTML=out;
+
+            out="<table>"
+            out="<tr><td><b>Image : </b></td><td><img src='"+site.favIconUrl+"' /></td></tr>"
+            out+="<tr><td><b>Title :</b></td><td>"+site.pagetitle+"</td></tr>";
+            out+="<tr><td><b>Url  : </b></td><td>"+site.url+"</td></tr>";
+
+            desc.innerHTML=out;
+
+            button.onclick=function(){
+                type=site.value;
+                json={
+                    'share': 'done',
+                    type:type,
+                    msg:msg.value,
+                    url:site.url
+                }
+                chrome.extension.sendRequest(json, sucess);
+                function sucess(back){
+                    alert(back)
+                    script.fade(-1)
+                }
+            }*/

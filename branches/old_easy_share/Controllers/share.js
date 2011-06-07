05e9f3c3ca3af956e23339127a6f3e12aa551f48
mohamedaliazouz <mohamedaliazouz@7c063873-07dd-0e2c-0e23-6587fe7ed649>
2011-06-07 09:31:55 +0000

    new working version

---
+++ branches/old_easy_share/Controllers/share.js
@@ -0,0 +1,40 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//var background=chrome.extension.getBackgroundPage();
+SharingShare={
+    site:"",
+    init:function(){
+        site=JSON.parse(localStorage.onclickedcontext);
+        out="</br><a href='' class='f-r'><img src='images/"+site.value+".png' width='31' height='32' alt='"+site.value+"' /></a>";
+        out+="<a href='' class='"+site.value+" f'>"+site.name+"</a>";
+        if(site.text){
+            $("#msg").val(site.text);
+        }
+        
+        $("#shareitem").html(out);
+        out="<table>"
+        out="<tr><td><b>Image : </b></td><td><img src='"+site.favIconUrl+"' /></td></tr>"
+        out+="<tr><td><b>Title :</b></td><td>"+site.pagetitle+"</td></tr>";
+        out+="<tr><td><b>Url  : </b></td><td>"+site.url+"</td></tr>";
+        $("#desc").html(out);
+    },
+    share:function(){
+        type=site.value;
+        json={
+            type:type
+        }
+        if(type=="twitter"){
+            json.link=SharingStaticData.twitterUpdatestatus;
+        }
+       ShareLinksBG.share($("#msg").val(), site.url,json);
+        
+    }
+}
+
+$(function(){
+    SharingShare.init();
+});
+
+

b518cb3ece81d3e782078875dc1564b59c3a9864
Mohamed Azouz <mohamedaliazouz@gmail.com>
2011-06-08 13:27:56 +0000

    Beta version without Gmail

--- views/content_script/contentscript.js
+++ views/content_script/contentscript.js
@@ -1,8 +1,15 @@
 script={
     getimage:function(){
+        var image="";
+        for(i=0;i<document.images.length;i++){
+            image=document.images[i].src;
+            if(image.substr(image.length-3, image.length) == "jpg"){
+                break;
+            }
+        }
         json={
             "getimage":"ok",
-            "image":document.getElementsByTagName("img")[0].src
+            "image":image
         }
         chrome.extension.sendRequest(json,function(){});
     }
